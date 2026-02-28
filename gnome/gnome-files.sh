fc9b7c861908433193f8068831e3af32c6084c9b
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-02-28 10:02:08 +0100

    y

---
+++ gnome/gnome-files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+in="sudo pacman -S --noconfirm --needed"
+aur="yay -S --noconfirm --needed"
+
+$in gnome-tweaks
+$in extensions-manager
+
+#GNOME EXTENSIONS
+Dash2Dock Animated - icedman
+User themes
+user themesX
+Alphabetical App Grid
+Blur my Shell
+Caffeine
+AppIndicator and KStatusNotifierItem Support
+Just Perfection
+
+
+$aur yaru-icon-theme
+$aut ttf-ubuntu-font-family
+$aur gtk-engine-murrine
